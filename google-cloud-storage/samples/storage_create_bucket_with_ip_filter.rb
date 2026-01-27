6c1fe37b3a27387776896c1789ca21259df74157
Shubhangi Singh <shubhangisin@google.com>
2026-01-27 10:37:13 +0000

    wip

--- google-cloud-storage/samples/storage_create_bucket_with_ip_filter.rb
+++ google-cloud-storage/samples/storage_create_bucket_with_ip_filter.rb
@@ -30,10 +30,13 @@ def create_bucket_with_ip_filter bucket_name:
   ),
    allow_all_service_agent_access: false
   )
+
   bucket = storage.create_bucket bucket_name do |b|
     b.ip_filter = ip_filter
     b.uniform_bucket_level_access = true
   end
+
+  binding.pry
 
   puts "Created bucket #{bucket_name} with IP filter enabled."
 end
@@ -73,47 +76,82 @@ def update_bucket_with_ip_filter bucket_name:
   ),
    allow_all_service_agent_access: false
   )
-  # require "google/cloud/iam"
-  # Initialize the IAM Admin Client (Not Credentials Client)
-  # iam_client = Google::Cloud::Iam::Service.new
-  client = Google::Cloud::ResourceManager::V3::Projects::Client.new
+  
+# require "google/cloud/resource_manager"
+
+# Direct client initialization
+client = Google::Cloud::ResourceManager::V3::Projects::Client.new
+
+project_id = storage.project
+resource_name = "projects/#{project_id}"
+
+# 1. Get the current polGoogle::Cloud::Iam::V1::IAM::Client.newicy
+# binding.pry
+policy = client.get_iam_policy resource: resource_name
+# binding.pry
+
+custom_role = "storage.buckets.exemptFromIpFilter"
+custom_role_path = "#{resource_name}/roles/#{custom_role}"
+service_account = "serviceAccount:#{storage.service_account_email}"
+#{serviceAccount:storage-sdk-vendor@storage-sdk-vendor.iam.gserviceaccount.com}
+
+binding.pry
+custom_role_binding = Google::Iam::V1::Binding.new(
+  role: custom_role_path,
+    members: [service_account]
+)
+policy.bindings << custom_role_binding
+
+client.set_iam_policy resource: resource_name, policy: policy
+# Google::Cloud::PermissionDeniedError: 7:Permission 'resourcemanager.projects.setIamPolicy' denied on resource '//cloudresourcemanager.googleapis.com/projects/storage-sdk-vendor' (or it may not exist).. debug_error_string:{UNKNOWN:Error received from peer ipv4:74.125.23.95:443 {grpc_message:"Permission \'resourcemanager.projects.setIamPolicy\' denied on resource \'//cloudresourcemanager.googleapis.com/projects/storage-sdk-vendor\' (or it may not exist).", grpc_status:7}} (Google::Cloud::PermissionDeniedError)
 
 
-  project_id = "storage-sdk-vendor"
-  role_id    = "GcsIpExemptRole"
+#   # Optional: Log the change
+#   puts "Updating project policy to include #{custom_role} for #{service_account}"
+# end
 
-  # Define the Custom Role
+puts "Project policy updated successfully."
+
+  # project_id = "storage-sdk-vendor"
+  # role_id    = "GcsIpExemptRole"
+  # binding.pry
+
+  # # Define the Custom Role
   # role = Google::Cloud::Iam::V1::Role.new(
   #   title: "GCS IP Exemption Role",
   #   included_permissions: ["storage.buckets.exemptFromIpFilter"],
   #   stage: :GA
   # )
-  resource = "projects/#{project_id}"
+  # resource = "projects/#{project_id}"
+  # storage_project = storage.project
+  # resource = "projects/#{storage_project}/roles/#{role_id}"
+  # storage_project.policy do |p|
+  #   p.add "roles/#{role}", member
+  # end
 
-    # Call test_iam_permissions. 
-    # In Ruby, you can pass a Hash that matches the request structure.
-    response = client.test_iam_permissions(
-      resource: resource,
-      permissions: ["storage.buckets.exemptFromIpFilter"]
-    )
+  #   # Call test_iam_permissions. 
+  #   # In Ruby, you can pass a Hash that matches the request structure.
+  #   response = client.test_iam_permissions(
+  #     resource: resource,
+  #     permissions: ["storage.buckets.exemptFromIpFilter"]
+  #   )
 
-    # The response object contains a 'permissions' array of granted permissions
-    puts "Granted permissions: #{response.permissions}"
+  #   # The response object contains a 'permissions' array of granted permissions
+  #   puts "Granted permissions: #{response.permissions}"
     
-    response.permissions
-    binding.pry
+  #   response.permissions
 
-      # role   = "projects/#{storage.project}/roles/GcsIpExemptRole"
-    member = "serviceAccount:insecure-cloudtop-shared-user@cloudtop-prod-asia-east.iam.gserviceaccount.com"
+  #     # role   = "projects/#{storage.project}/roles/GcsIpExemptRole"
+  #   member = "serviceAccount:insecure-cloudtop-shared-user@cloudtop-prod-asia-east.iam.gserviceaccount.com"
 
-    bucket.policy requested_policy_version: 3 do |policy|
-      policy.bindings.insert role: role, members: [member]
-    end
-    # bucket = storage.create_bucket bucket_name 
-    #    bucket.policy do |p|
-    #     p.add "roles/storage.buckets.exemptFromIpFilter", "serviceAccount:insecure-cloudtop-shared-user@cloudtop-prod-asia-east.iam.gserviceaccount.com"
-    #   end
-    #   binding.pry
+  #   bucket.policy requested_policy_version: 3 do |policy|
+  #     policy.bindings.insert role: "roles/storage.buckets.exemptFromIpFilter", members: ["serviceAccount:insecure-cloudtop-shared-user@cloudtop-prod-asia-east.iam.gserviceaccount.com"]
+  #   end
+  #   # bucket = storage.create_bucket bucket_name 
+  #   #    bucket.policy do |p|
+  #   #     p.add "roles/storage.buckets.exemptFromIpFilter", "serviceAccount:insecure-cloudtop-shared-user@cloudtop-prod-asia-east.iam.gserviceaccount.com"
+  #   #   end
+  #   #   binding.pry
 
   bucket.update do |b|
     b.ip_filter = ip_filter
